7eb71a568eb28881dedf20721ea11c625a932aec
Mooney <zblankenship@una.edu>
2024-01-25 16:43:29 -0600

    bamboozled

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5500;
+// set the viuew engine to ejs
+app.set('view engine', 'ejs')
+
+
+let myTypeServer = 'The General';
 
 app.get('/', function (req, res) {
-  res.send('Hello World From Express')
+  
+    res.render('\index.ejs', {
+
+      myTypeClient: myTypeServer
+    })
 })
 
+// app.listen(3000)
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(`november app listening on port ${port}`)
+})
